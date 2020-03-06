1a96e6eec720010e7bab3bb35f9c4aa3f01829fd
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-03-06 00:44:39 -0600

    Rutas de la asministración general de la plataforma.

--- liebe-cms-project/src/app/app-routing.module.ts
+++ liebe-cms-project/src/app/app-routing.module.ts
@@ -3,30 +3,45 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccesoComponent } from './acceso/acceso.component';
 import { BodyLandingComponent } from './body-landing/body-landing.component';
 import { RegistroComponent } from './registro/registro.component';
+import { AdminPrincipalComponent } from './Administracion/admin-principal/admin-principal.component';
+import { InicioComponent } from './Administracion/inicio/inicio.component';
+import { AppComponent } from './app.component';
+
 
 
 const routes: Routes = [
   {
-    path: 'acceso',
-    component: AccesoComponent 
-  },
-  {
     path: '',
-    component: BodyLandingComponent
+    component: BodyLandingComponent,
   },
   {
-    path:'inicio',
-    component: BodyLandingComponent 
+    path: 'acceso',
+    component: AccesoComponent
   },
   {
     path: 'registro',
     component: RegistroComponent
-  }
-
+  },
+  {
+    path: 'inicio',
+    component: BodyLandingComponent
+  },
+  {
+    path: 'admin',
+    component: AdminPrincipalComponent,
+    children:[
+      {
+        path: 'inicio',
+        component: InicioComponent
+      }
+    ]
+}
 ];
 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: 
+  [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- liebe-cms-project/src/app/app.module.ts
+++ liebe-cms-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +9,11 @@ import { BodyLandingComponent } from './body-landing/body-landing.component';
 import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 import { AccesoComponent } from './acceso/acceso.component';
 import { RegistroComponent } from './registro/registro.component';
+import { AdminPrincipalComponent } from './Administracion/admin-principal/admin-principal.component';
+import { PanelComponent } from './Administracion/panel/panel.component';
+import { InicioComponent } from './Administracion/inicio/inicio.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,11 +22,16 @@ import { RegistroComponent } from './registro/registro.component';
     BodyLandingComponent,
     PiePaginaComponent,
     AccesoComponent,
-    RegistroComponent
+    RegistroComponent,
+    AdminPrincipalComponent,
+    PanelComponent,
+    InicioComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
