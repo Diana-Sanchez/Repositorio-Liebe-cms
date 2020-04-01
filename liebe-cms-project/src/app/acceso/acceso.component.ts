1bcaf59cf3aeba6ec7bc583ff75bdb25fd47d7df
Yolibeth Sánchez <yolibeth.diana@gmail.com>
2020-03-31 23:56:33 -0600

    Validaciones de acceso tipo usuario normal.

--- liebe-cms-project/src/app/acceso/acceso.component.ts
+++ liebe-cms-project/src/app/acceso/acceso.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-acceso',
@@ -6,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acceso.component.css']
 })
 export class AccesoComponent implements OnInit {
+  // Creando instancia del tipo FormGroup para el acceso de usuarios normales.
+  acessoUsuarioNormal = new FormGroup({
+    nombreDeUsuario:new FormControl('', [Validators.required, Validators.maxLength(8)]),
+    passwordDeUsuario:new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)])
+  });
 
+  usuariosNormalesIngresados: any = [];
+
+  get nombreDeUsuario(){
+    return this.acessoUsuarioNormal.get('nombreDeUsuario');
+  };
+
+  get passwordDeUsuario(){
+    return this.acessoUsuarioNormal.get('passwordDeUsuario');
+  };
+
+
+  guardarAccesoUsuarioNormal(){
+    
+    this.usuariosNormalesIngresados.push(this.acessoUsuarioNormal.value);
+    console.log(this.usuariosNormalesIngresados);
+    console.log('Fomulario válido:', this.acessoUsuarioNormal.valid );
+    this.acessoUsuarioNormal.reset(); 
+  };
   constructor() { }
 
   ngOnInit(): void {
